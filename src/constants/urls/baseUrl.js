92f776f8f5b357f8c71f42fc022839efad897b26
Marcin Goszczynski <61630391+mgoszcz@users.noreply.github.com>
2025-02-25 15:26:04 +0100

    add production url (#14)

--- src/constants/urls/baseUrl.js
+++ src/constants/urls/baseUrl.js
@@ -4,6 +4,8 @@ export const getBackendBaseUrl = () => {
     return "http://localhost:3001";
   } else if (environment === "development") {
     return "https://shopping-list-be-development.up.railway.app";
+  } else if (environment === "production") {
+    return "https://shopping-list-be.up.railway.app";
   } else {
     throw new Error(`Unknown environment: ${environment}`);
   }
