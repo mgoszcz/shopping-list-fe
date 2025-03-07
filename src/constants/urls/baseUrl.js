81c8705b9815feb4e5a5926cc718c3018976678c
Marcin Goszczynski <61630391+mgoszcz@users.noreply.github.com>
2025-03-07 14:15:09 +0100

    change footer color and add env name if not production (#21)

--- src/constants/urls/baseUrl.js
+++ src/constants/urls/baseUrl.js
@@ -1,12 +1,13 @@
+const ENVIRONMENT = process.env.REACT_APP_ENVIRONMENT;
+
 export const getBackendBaseUrl = () => {
-  const environment = process.env.REACT_APP_ENVIRONMENT || "prod";
-  if (environment === "local") {
+  if (ENVIRONMENT === "local") {
     return "http://localhost:3001";
-  } else if (environment === "development") {
+  } else if (ENVIRONMENT === "development") {
     return "https://shopping-list-be-development.up.railway.app";
-  } else if (environment === "production") {
+  } else if (ENVIRONMENT === "production") {
     return "https://shopping-list-be.up.railway.app";
   } else {
-    throw new Error(`Unknown environment: ${environment}`);
+    throw new Error(`Unknown environment: ${ENVIRONMENT}`);
   }
 };
